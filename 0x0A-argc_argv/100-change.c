df397add98dca4cf1ee507978f8f1eaf81be6d95
Linnnetteseven <mugwanjalk@gmail.com>
2023-11-17 12:10:24 -0800

    update argv and arg c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 		printf(" Error\n");
@@ -31,12 +32,12 @@ int main(int argc, char *argv[])
 
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
-		while(num >= coins[j])
+		while (num >= coins[j])
 		{
 			result++;
 			num -= coins[j];
 		}
 	}
-	printf("%d\n",result);
+	printf("%d\n", result);
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -54,17 +54,15 @@ int main(int argc, char *argv[])
 			sum += str_to_int;
 																							}
 
-											/*Condition if one of the number contains symbols that are not digits*/
-		
+	/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		
 		count++;
 	}
-	
+
 	printf("%d\n", sum); /*print sum*/
 
 	return (0);
